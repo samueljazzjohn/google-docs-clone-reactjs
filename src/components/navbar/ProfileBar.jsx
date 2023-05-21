87869968569a754c92d9d237bdd65637f0d471be
samueljazzjohn <samueljazzjohn@gmail.com>
2023-05-21 22:26:30 +0530

    cursor pointer added

--- src/components/navbar/ProfileBar.jsx
+++ src/components/navbar/ProfileBar.jsx
@@ -4,15 +4,15 @@ const ProfileBar = () => {
   return (
     <>
       <div className="flex flex-row space-x-5 mt-2">
-        <span class="material-symbols-outlined">history</span>
-        <span class="material-symbols-outlined">videocam</span>
+        <span class="material-symbols-outlined cursor-pointer">history</span>
+        <span class="material-symbols-outlined cursor-pointer">videocam</span>
 
         <div className="flex flex-row space-x-1">
-          <span class="material-symbols-outlined">chat</span>
-          <span class="material-symbols-outlined">arrow_drop_down</span>
+          <span class="material-symbols-outlined cursor-pointer">chat</span>
+          <span class="material-symbols-outlined cursor-pointer">arrow_drop_down</span>
         </div>
-        <div className="flex flex-row bg-blue-200 rounded-3xl px-5 py-2 -translate-y-2 space-x-2">
-          <span class="material-symbols-outlined text-xl">lock</span>
+        <div className="flex flex-row bg-blue-200 rounded-3xl px-5 py-2 -translate-y-2 space-x-2 cursor-pointer">
+          <span class="material-symbols-outlined cursor-pointer text-xl">lock</span>
           <p className="text-[14px] font-semibold translate-y-1">Share</p>
         </div>
         <img
