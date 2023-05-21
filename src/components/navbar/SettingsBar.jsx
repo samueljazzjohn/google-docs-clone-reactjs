e8370ae495f08704b324b34e4a5ccb48b29ee427
samueljazzjohn <samueljazzjohn@gmail.com>
2023-05-21 22:27:34 +0530

    hover effect added

--- src/components/navbar/SettingsBar.jsx
+++ src/components/navbar/SettingsBar.jsx
@@ -3,29 +3,29 @@ import React from 'react'
 const SettingsBar = () => {
   return (
     <nav className='ml-2'>
-      <ul className='flex flex-row space-x-5 text-[14px] font-normal text-text'>
-        <li>
+      <ul className='flex flex-row space-x-2 text-[14px] font-normal text-text'>
+        <li className='py-[2px] px-2 rounded-smdm hover:bg-gray-100'>
           <a href="/">File</a>
         </li>
-        <li>
+        <li className='py-[2px] px-2 rounded-md hover:bg-gray-100'>
           <a href="/">Edit</a>
         </li>
-        <li>
+        <li className='py-[2px] px-2 rounded-md hover:bg-gray-100'>
           <a href="/">View</a>
         </li>
-        <li>
+        <li className='py-[2px] px-2 rounded-md hover:bg-gray-100'>
           <a href="/">Insert</a>
         </li>
-        <li>
+        <li className='py-[2px] px-2 rounded-md hover:bg-gray-100'>
           <a href="/">Format</a>
         </li>
-        <li>
+        <li className='py-[2px] px-2 rounded-md hover:bg-gray-100'>
           <a href="/">Tools</a>
         </li>
-        <li>
+        <li className='py-[2px] px-2 rounded-md hover:bg-gray-100'>
           <a href="/">Extensions</a>
         </li>
-        <li>
+        <li className='py-[2px] px-2 rounded-md hover:bg-gray-100'>
           <a href="/">Help</a>
         </li>
       </ul>
